311cb6b63e443e1e63b97f15fd73208ed814cc3f
Andrew Lendacky <lendackya@Andrews-MacBook-Air-4.local>
2016-08-11 13:19:21 -0400

    Add Validations to Course

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,6 @@
 class Course < ApplicationRecord
   belongs_to :user
+
+  validates :starts_time, :ends_time, :user_id, :name, presence: true
+
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+let(:course) { FactoryGirl.create(:course) }
+
+  # it "Has a valid factory" do
+  #   expect(course).to be_valid
+  # end
+
+  describe "Validations" do
+
+    it "does not allow empty start time" do
+      expect(FactoryGirl.build(:course, starts_time: nil)).to_not be_valid
+    end
+
+    it "does not allow empty end time" do
+      expect(FactoryGirl.build(:course, ends_time: nil)).to_not be_valid
+    end
+
+    it "does not allow empty user id" do
+      expect(FactoryGirl.build(:course, user_id: nil)).to_not be_valid
+    end
+
+    it "does not allow empty name" do
+      expect(FactoryGirl.build(:course, name: nil)).to_not be_valid
+    end
+  end
 end
